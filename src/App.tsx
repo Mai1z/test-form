bde2662ce4a6dd3117de14f364d152e655fd3b15
Artem Prytkin <prytkinart@mail.ru>
2021-10-16 13:05:18 +0400

    add base layout

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <>
+    123
+  </>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
